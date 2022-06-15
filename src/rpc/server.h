a17e652abaf2cc2779b98917423bfa33c1eb36f5
volbil <volbilnexus@gmail.com>
2022-06-15 22:32:23 +0300

    Governance init

--- src/rpc/server.h
+++ src/rpc/server.h
@@ -270,6 +270,7 @@ extern UniValue getchaintips(const JSONRPCRequest& request);
 extern UniValue invalidateblock(const JSONRPCRequest& request);
 extern UniValue reconsiderblock(const JSONRPCRequest& request);
 extern UniValue getblockindexstats(const JSONRPCRequest& request);
+extern UniValue issuanceinfo(const JSONRPCRequest& request);
 extern UniValue getserials(const JSONRPCRequest& request);
 extern void validaterange(const UniValue& params, int& heightStart, int& heightEnd, int minHeightStart=1);
 
