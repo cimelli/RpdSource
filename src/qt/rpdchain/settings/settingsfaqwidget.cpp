c4f23502964e946b6b8253ec3f14ee0e52429c01
cimelli <83247734+cimelli@users.noreply.github.com>
2023-12-01 14:36:41 +0000

    Qt update
    update to welcome and texts

--- src/qt/rpdchain/settings/settingsfaqwidget.cpp
+++ src/qt/rpdchain/settings/settingsfaqwidget.cpp
@@ -17,9 +17,9 @@ SettingsFaqWidget::SettingsFaqWidget(RPDCHAINGUI *parent) :
 
 #ifdef Q_OS_MAC
     ui->container->load("://bg-welcome");
-    setCssProperty(ui->container, "container-welcome-no-image");
+    setCssProperty(ui->container, "bg-welcome");
 #else
-    setCssProperty(ui->container, "container-welcome");
+    setCssProperty(ui->container, "bg-welcome");
 #endif
     setCssProperty(ui->labelTitle, "text-title-faq");
     setCssProperty(ui->labelWebLink, "text-content-white");
